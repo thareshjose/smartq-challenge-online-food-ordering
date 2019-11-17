cf9d6ff86190b042c3a3e135f03bba21e9263fa7
Tharesh <99tharesh@gmail.com>
2019-11-18 00:41:47 +0530

    Updates

---
+++ precache-manifest.b679d52f394ee99664cbae11f754318f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b1aaa54ad316f92c09e56ab9fb5fbb",
+    "url": "/smartq-challenge-online-food-ordering/index.html"
+  },
+  {
+    "revision": "bd544b5002d6eaf6b91f",
+    "url": "/smartq-challenge-online-food-ordering/static/css/main.67443316.chunk.css"
+  },
+  {
+    "revision": "2c982c6a2188d85afc51",
+    "url": "/smartq-challenge-online-food-ordering/static/js/2.7913d77a.chunk.js"
+  },
+  {
+    "revision": "bd544b5002d6eaf6b91f",
+    "url": "/smartq-challenge-online-food-ordering/static/js/main.e2d53d34.chunk.js"
+  },
+  {
+    "revision": "ed2bb1b10e63d846b692",
+    "url": "/smartq-challenge-online-food-ordering/static/js/runtime-main.357fd1c5.js"
+  }
+]);
